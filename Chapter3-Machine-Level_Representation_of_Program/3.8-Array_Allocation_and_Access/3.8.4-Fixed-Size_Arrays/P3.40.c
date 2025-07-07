1593092d244c818cd10157f897d0e2d76737ed74
Evoque <rubievoque@gmail.com>
2025-07-07 19:52:39 +0800

    finish: 3.8.4-Fixed-Size_Arrays

--- Chapter3-Machine-Level_Representation_of_Program/3.8-Array_Allocation_and_Access/3.8.4-Fixed-Size_Arrays/P3.40.c
+++ Chapter3-Machine-Level_Representation_of_Program/3.8-Array_Allocation_and_Access/3.8.4-Fixed-Size_Arrays/P3.40.c
@@ -11,12 +11,41 @@ void fix_set_diag(fix_matrix A, int val) {
     }
 }
 
-/** Optimized C code */
+/** Optimized C code 1 */
+// void fix_set_diag_opt(fix_matrix A, int val) {
+//     int *Ptr = &A[0][0];
+//     int *Bend = &A[N][N];
+//     do {
+//         *Ptr = val;
+//         Ptr += 68;
+//     } while (Ptr != Bend);
+// }
+
+/** Optimized C code 2 */
 void fix_set_diag_opt(fix_matrix A, int val) {
-    int *Ptr = &A[0][0];
-    int *Bend = &A[N][N];
+    int *Abase = &A[0][0];
+    // i 作为要赋值的matrix的索引
+    int i = 0;
+    long iEnd = N * (N + 1);
     do {
-        *Ptr = val;
-        Ptr += 68;
-    } while (Ptr != Bend);
+        Abase[i] = val;
+        i += N + 1;
+    } while (i < N);
+}
+
+void test_array_pointer(fix_matrix A){
+    int *Abase = &A[0][0];
+
+    printf("A: %p\n", A);
+    printf("Abase: %p\n", Abase);
+
+    printf("A: %p\n", &A[0][1]);
+    printf("Abase[1]: %p\n", &Abase[1]);
+}
+
+int main() {
+    fix_matrix A = {{0, 0}, {1, 1}};
+    test_array_pointer(A);
+
+    return 0;
 }
