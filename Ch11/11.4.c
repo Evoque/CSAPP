749cdc2011862342cd456b8f4befa0a427ea42bf
Evoque <rubievoque@gmail.com>
2025-07-16 23:47:08 +0800

    work! 11.4

---
+++ Ch11/11.4.c
@@ -0,0 +1,39 @@
+
+#include "csapp.h"
+#include <netdb.h>
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+    printf("================ start ================\n");
+    struct addrinfo *p, *listp, hints;
+    char buf[MAXLINE];
+    // int rc, flags;
+
+    if (argc != 2) {
+        fprintf(stderr, "usage: %s <domain name>\n", argv[0]);
+        exit(0);
+    }
+
+    /* Get a list of addrinfo records */
+    memset(&hints, 0, sizeof(struct addrinfo));
+    hints.ai_family = AF_INET;         /* IPv4 only */
+    hints.ai_socktype = SOCK_STREAM;   /* Connections only */
+    int rc = getaddrinfo(argv[1], NULL, &hints, &listp);
+    if (rc != 0) {
+        fprintf(stderr, "getaddrinfo error: %s\n", gai_strerror(rc));
+        exit(1);
+    }
+
+    /* Walk the list and display each IP address */
+    int flags = NI_NUMERICHOST;  /* Display address string instead of domain name */
+    for (p = listp; p; p = p->ai_next) {
+        getnameinfo(p->ai_addr, p->ai_addrlen, buf, MAXLINE, NULL, 0, flags);
+        printf("%s\n", buf);
+    }
+
+    /* Clean up */
+    freeaddrinfo(listp);
+    
+
+    exit(0);
+}
--- TODO.md
+++ TODO.md
@@ -2,3 +2,4 @@
 - [ ] 远程调试: gdb + gdbserver | lldb/lldb-server(Scode中的LLDB)(http://lldb.llvm.org/use/map.html)
 - [ ] 生成一个类中函数间的调用关系、变量的引用关系？推广到整个module、package、工程。 
 - [ ] 搞一个「从0开始写Web服务器」from scratch
+- [ ] 数据库连接（数据库连接池）的原理，数据库连接是什么通信？
